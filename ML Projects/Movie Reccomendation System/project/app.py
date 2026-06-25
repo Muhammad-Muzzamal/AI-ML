f3ba08932227a47b67bfc3706a38fd04ec2390f9
Muhammad Muzzamal <ashrafmuzzamal7@gmail.com>
2026-06-25 07:12:44 +0500

    project progress

--- ML Projects/Movie Reccomendation System/project/app.py
+++ ML Projects/Movie Reccomendation System/project/app.py
@@ -4,9 +4,18 @@ import pickle
 movies_list = pickle.load(open("movies.pkl", "rb"))
 movies_list = movies_list["title"].values
 
+
+
 st.title("Movie Recommender System")
 
-option = st.selectbox(
+
+
+selected_movies_name = st.selectbox(
     "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+    movies_list
 )
+
+
+if st.button("Recommend"):
+    recommend(selected_movies_name)
+    st.write(selected_movies_name)
